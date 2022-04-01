95f4d3dc340e5e7f779962fe9b1f44a0f18e64d4
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-01 15:27:10 +0530

    modified the playRound function in jscript.js

--- jscript.js
+++ jscript.js
@@ -1,19 +1,27 @@
+
+var arr = ['rock', 'paper', 'scissors']
 function computerPlay(){
-    const arr = ['rock', 'paper', 'scissors']
     let i = Math.floor(Math.random() * 3);
+    
     return arr[i];
 }
 
 let a = computerPlay()
-console.log(a)
+console.log('computer',a)
 
 function playRound(x,y){
-    if (x == 'rock' && y == 'rock'){
+    if (x == 'rock' && y == 'rock' || x == 'paper' && y == 'paper' || x == 'scissors' && y == 'scissors'){
         console.log('its a tie')
     }
-    else if (x == 'paper' && y == 'rock'){
+    else if (x == 'paper' && y == 'rock' || x == 'rock' && y == 'scissors' || x == 'scissors' && y == 'paper'){
         console.log('you loose!')
     }
+    else if (x == 'paper' && y == 'scissors' || x == 'scissors' && y == 'rock' || x == 'rock' && y == 'paper'){
+        console.log('you own')
+    }
+    
 }
-b = 'rock'
+
+b = arr[Math.floor(Math.random() * 3)]
+console.log('u',b)
 playRound(a,b)
