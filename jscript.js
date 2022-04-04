1b7edcf6308ff3b07307baf6761ed6278ff519e8
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-04 10:50:53 +0530

    added three button and the necessary function for thebutton

--- jscript.js
+++ jscript.js
@@ -9,31 +9,66 @@ function computerPlay(){
 // comparing all inputs and returnig the result
 function playRound(x,y){
     if (x == 'rock' && y == 'rock' || x == 'paper' && y == 'paper' || x == 'scissors' && y == 'scissors'){
-        const result1 = 'tie'
+        const result1 = 'Its a tie'
         //console.log(result1)
         return result1;
     }
     else if (x == 'paper' && y == 'rock' || x == 'rock' && y == 'scissors' || x == 'scissors' && y == 'paper'){
-        const result2 = 'loose'
+        const result2 = 'you are a loser'
         //console.log(result2)
         return result2;
     }
     else if (x == 'paper' && y == 'scissors' || x == 'scissors' && y == 'rock' || x == 'rock' && y == 'paper'){
-        const result3 = 'own'
+        const result3 = 'you are a winner'
         //console.log(result3)
         return result3;
     }
     
 }
 
+
+
+function rock(){
+    b = document.querySelector('#test1').innerHTML = 'rock';
+    a = computerPlay()
+    document.querySelector('#test').innerHTML = a;
+    //run the game function
+    g = playRound(a,b);
+    document.querySelector('.result').innerHTML = g;
+    console.log(g)
+}
+ //created the paper function
+function paper(){
+    b = document.querySelector('#test1').innerHTML = 'paper';
+    a = computerPlay()
+    document.querySelector('#test').innerHTML = a;
+    //run the game function
+    g = playRound(a,b);
+    document.querySelector('.result').innerHTML = g;
+    console.log(g)
+}
+// cerated scissors function
+function scissors(){
+    b = document.querySelector('#test1').innerHTML = 'scissors';
+    a = computerPlay()
+    document.querySelector('#test').innerHTML = a;
+    //run the game function
+    g = playRound(a,b);
+    document.querySelector('.result').innerHTML = g;
+    console.log(g)
+}
+
+/*
 //comparing the actual inputs users and computers
 var count = 0;
-for (n=0; n<5; n++){
-    input = prompt('please enter ur choise rock/scissors/paper : ')
+for (n=0; n<1; n++){
+    input = 'rock' //prompt('please enter ur choise rock/scissors/paper : ')
     b = input.toLocaleLowerCase()
     console.log('your choise',b)
     a = computerPlay()
     console.log('computer choise',a)
+
+    // main function to compare input and output result
     var result = playRound(a,b)
     //console.log(result)
     if(result == 'own'){
@@ -53,6 +88,7 @@ for (n=0; n<5; n++){
     }
 }
 
+/*
 // rpinting the final result
 console.log('your final score is :',count)
 if(count <0){
@@ -64,4 +100,4 @@ else if(count >0){
 else{
     console.log('its a tie')
 }
-
+*/
