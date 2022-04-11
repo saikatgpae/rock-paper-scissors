d7c85b18b8497cb01fab874407139b76059212f9
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-11 23:34:44 +0530

    added style.css added the style to the index.html

--- jscript.js
+++ jscript.js
@@ -5,22 +5,26 @@ function computerPlay(){
     return arr[Math.floor(Math.random() * 3)];
 }
 
+// intializing the counter into 0 and final result as 0
+var counter = 0;
+var final_result = 0;
 
 // comparing all inputs and returnig the result
 function playRound(x,y){
     if (x == 'rock' && y == 'rock' || x == 'paper' && y == 'paper' || x == 'scissors' && y == 'scissors'){
-        const result1 = 'Its a tie'
+        const result1 = 'tie'
         //console.log(result1)
         return result1;
     }
     else if (x == 'paper' && y == 'rock' || x == 'rock' && y == 'scissors' || x == 'scissors' && y == 'paper'){
-        const result2 = 'you are a loser'
+        const result2 = 'loser'
         //console.log(result2)
         return result2;
     }
     else if (x == 'paper' && y == 'scissors' || x == 'scissors' && y == 'rock' || x == 'rock' && y == 'paper'){
-        const result3 = 'you are a winner'
+        const result3 = 'winner'
         //console.log(result3)
+        final_result ++;
         return result3;
     }
     
@@ -29,34 +33,86 @@ function playRound(x,y){
 
 
 function rock(){
+    if(counter == 0){
+        document.querySelector('.final-result').innerHTML = '';
+        document.querySelector('.final-result-text').innerHTML = '';    
+    }
+    counter ++;
+    document.querySelector('.counter').innerHTML = counter;
     b = document.querySelector('#test1').innerHTML = 'rock';
     a = computerPlay()
     document.querySelector('#test').innerHTML = a;
     //run the game function
     g = playRound(a,b);
     document.querySelector('.result').innerHTML = g;
-    console.log(g)
+    document.querySelector('.final-result').innerHTML = final_result;
+    if(counter == 5){
+        if(final_result >= 3){
+            document.querySelector('.final-result').innerHTML = final_result;
+            document.querySelector('.final-result-text').innerHTML = 'You won the game';   
+        }else{
+            document.querySelector('.final-result').innerHTML = final_result;
+            document.querySelector('.final-result-text').innerHTML = 'You lost, Try again';   
+        }
+        counter = 0;
+        final_result = 0;
+        document.querySelector('.counter').innerHTML = counter;
+    }
 }
  //created the paper function
 function paper(){
+    if(counter == 0){
+        document.querySelector('.final-result').innerHTML = '';
+        document.querySelector('.final-result-text').innerHTML = '';    
+    }
+    counter ++;
+    document.querySelector('.counter').innerHTML = counter;
     b = document.querySelector('#test1').innerHTML = 'paper';
     a = computerPlay()
     document.querySelector('#test').innerHTML = a;
     //run the game function
     g = playRound(a,b);
     document.querySelector('.result').innerHTML = g;
-    console.log(g)
+    document.querySelector('.final-result').innerHTML = final_result;
+    if(counter == 5){
+        if(final_result >= 3){
+            document.querySelector('.final-result-text').innerHTML = 'You won the game😍🏆';   
+        }else{
+            document.querySelector('.final-result-text').innerHTML = 'You lost the game, Try again😔';   
+        }
+        counter = 0;
+        final_result = 0;
+        document.querySelector('.counter').innerHTML = counter;
+    }
 }
 // cerated scissors function
 function scissors(){
+    if(counter == 0){
+        document.querySelector('.final-result').innerHTML = '';
+        document.querySelector('.final-result-text').innerHTML = '';    
+    }
+    counter ++;
+    document.querySelector('.counter').innerHTML = counter;
     b = document.querySelector('#test1').innerHTML = 'scissors';
     a = computerPlay()
     document.querySelector('#test').innerHTML = a;
     //run the game function
     g = playRound(a,b);
     document.querySelector('.result').innerHTML = g;
-    console.log(g)
+    document.querySelector('.final-result').innerHTML = final_result;
+    if(counter == 5){
+        if(final_result >= 3){
+            document.querySelector('.final-result-text').innerHTML = 'You won the game';   
+        }else{
+            document.querySelector('.final-result-text').innerHTML = 'You lost the game, Try again 😔';   
+        }
+        counter = 0;
+        final_result = 0;
+        document.querySelector('.counter').innerHTML = counter;
+    }
 }
+
+
 
 /*
 //comparing the actual inputs users and computers
